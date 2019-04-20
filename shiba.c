21686f5b623d9aaebc9ef908b46c89b6bf728025
shiba786 <49391275+shiba786@users.noreply.github.com>
2019-04-20 19:36:59 +0530

    Add files via upload

--- shiba.c
+++ shiba.c
@@ -3,20 +3,14 @@
 int main() 
 { 
       int i, limit, total = 0, x, counter = 0, time_quantum,j; //variable decaration
-      
 	  int wait_time = 0, turnaround_time = 0,pos,z,p[10],prio[10], a_time[10], b_time[10], temp[10],b; 
-      
 	  float average_wait_time, average_turnaround_time;
-      
 	  printf("\nEnter Total Number of Processes:"); //no of processes
-      
 	  scanf("%d", &limit); 
-      
 	  x = limit; 
       for(i = 0; i < limit; i++) 
       {
 	    p[i]=i+1;
-	   
 	    prio[i]=0;
             printf("\nEnter total Details of Process[%d]\n", i + 1);
             printf("Arrival Time:\t");
@@ -25,13 +19,11 @@ int main()
             scanf("%d", &b_time[i]); 
             temp[i] = b_time[i];
       }
-	   
       printf("\nEnter the Time Quantum:"); 
       scanf("%d", &time_quantum); 
       printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\t Priority\n");//display of value
       for(total = 0, i = 0; x != 0;) 
       { 
-
 		    for(z=0;z<limit;z++)
 		    {
 			int temp1;
@@ -40,25 +32,19 @@ int main()
 			{
 			    if(prio[j]<prio[pos])
 				pos=j;
-			}
-		 
+			} 
 		temp1=prio[z];
-	
 		prio[z]=prio[pos];
-	
 		prio[pos]=temp1; 
-		 
 			temp1=b_time[z];
 			b_time[z]=b_time[pos];
 			b_time[pos]=temp1;
 		 			temp1=a_time[z];
 				a_time[z]=a_time[pos];
 			a_time[pos]=temp1;
-
 			temp1=p[z];
 				p[z]=p[pos];
 			p[pos]=temp1;
-
 			temp1=temp[z];
 				temp[z]=temp[pos];
 			temp[pos]=temp1;
@@ -72,13 +58,11 @@ int main()
                   temp[i] = 0; 
                   counter = 1; 
             } 
-            
 			else if(temp[i] > 0) 
             { 
                   temp[i] = temp[i] - time_quantum; 
                   total = total + time_quantum; 
             } 
-
 	for(b=0;b<limit;b++)
 		{
 			if(b==i)
@@ -86,7 +70,6 @@ int main()
 			else
 			prio[b]+=2;
 		}
-
             if(temp[i] == 0 && counter == 1) 
             { 
                   x--; 
@@ -97,18 +80,15 @@ int main()
             } 
             if(i == limit - 1) 
             {
-                  i = 0; 
-            
+                  i = 0;    
 			}
             else if(a_time[i + 1] <= total) 
             {
                   i++;
-            
 			}
             else 
             {
                   i = 0;
-            
 			}		
       } 
       return 0; 
